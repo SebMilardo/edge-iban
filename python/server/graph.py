2f5f21111dbfdada26310b6ae3c560a6bdeae61c
Sebastiano Milardo <s.milardo@hotmail.it>
2018-04-07 11:32:01 +0200

    New Graphs

--- python/server/graph.py
+++ python/server/graph.py
@@ -4,54 +4,58 @@ import pandas as pd
 
 
 def graph():
-    df = pd.read_csv("data/edge.txt", header=-1)
-    df.reset_index(inplace=True)
-    df.columns = ("a","b","c")
-    df.b = df.b / 1000000
-    df = df[:900]
-    ax = df.b.hist(bins=np.arange(0, 125), normed=True)
-    ax.set_ylim([0,0.30])
-    ax.set_xlim([20, 125])
+    df_e = pd.read_csv("data/edge.txt", header=-1)
+    df_e.reset_index(inplace=True)
+    df_e.columns = ("a","b","c")
+    df_e = df_e.b / 2000000
+    df_e = df_e[:900]
+    ax = df_e.hist(bins=np.arange(0, 125), normed=True)
+    ax.set_ylim([0,0.40])
+    ax.set_xlim([0, 65])
     plt.xlabel("Delay (ms)")
     plt.savefig("edge.pdf")
     plt.show()
 
     plt.figure()
-    df = pd.read_csv("data/cloud.txt", header=-1)
-    df.reset_index(inplace=True)
-    df.columns = ("a","b","c")
-    df.b = df.b / 1000000
-    df = df[:900]
-    ax = df.b.hist(bins=np.arange(0, 125), normed=True)
-    ax.set_ylim([0,0.30])
-    ax.set_xlim([20, 125])
+    df_c = pd.read_csv("data/cloud.txt", header=-1)
+    df_c.reset_index(inplace=True)
+    df_c.columns = ("a","b","c")
+    df_c = df_c.b / 2000000
+    df_c = df_c[:900]
+    ax = df_c.hist(bins=np.arange(0, 125), normed=True)
+    ax.set_ylim([0,0.40])
+    ax.set_xlim([0, 65])
     plt.xlabel("Delay (ms)")
     plt.savefig("cloud.pdf")
     plt.show()
 
-    # plt.figure()
-    # df = pd.read_csv("data/bluetooth.txt", header=-1)
-    # df.reset_index(inplace=True)
-    # df.columns = ("a","b","c")
-    # df.b = df.b / 1000000000
-    # df = df[:900]
-    # df.b.hist(bins=np.arange(0, 0.2, 0.001), normed=True)
-    # plt.xlabel("Delay (s)")
-    # plt.savefig("cloud.pdf")
-    # plt.show()
+    plt.figure()
+    df_b = pd.read_csv("data/bluetooth.txt", header=-1)
+    df_b.reset_index(inplace=True)
+    df_b.columns = ("a","b","c")
+    df_b = df_b.c * 500
+    df_b = df_b[:900]
+    ax = df_b.hist(bins=np.arange(0, 125), normed=True)
+    ax.set_ylim([0,0.40])
+    ax.set_xlim([0, 65])
+    plt.xlabel("Delay (s)")
+    plt.savefig("cloud.pdf")
+    plt.show()
 
     plt.figure()
-    df = pd.read_csv("data/iban.txt", header=-1)
-    df.reset_index(inplace=True)
-    df.columns = ("a","b","c")
-    df = df[:900]
-    df.c = df.c * 1000
-    ax = df.c.hist(bins=np.arange(0, 125), normed=True)
-    ax.set_ylim([0,0.30])
-    ax.set_xlim([20, 125])
+    df_i = pd.read_csv("data/iban.txt", header=-1)
+    df_i.reset_index(inplace=True)
+    df_i.columns = ("a","b","c")
+    df_i = df_i[:900]
+    df_i = df_i.c * 500
+    ax = df_i.hist(bins=np.arange(0, 125), normed=True)
+    ax.set_ylim([0,0.40])
+    ax.set_xlim([0, 65])
     plt.xlabel("Delay (ms)")
     plt.savefig("iban.pdf")
     plt.show()
+
+
 
 
     pass
